8a3f8e2da55f9ccf549d5fc3989f36819a5f3532
Jairo Reyes Rosa <jareyes@nt.core>
2019-03-01 14:16:14 -0400

    fix: testing realtime database

--- src/store/actions/chatActions.js
+++ src/store/actions/chatActions.js
@@ -1,50 +1,54 @@
 import { createAction, createActionThunk } from '../utils/helpers/actionFactory';
 import firebaseService from '../../utils/services/firebaseService';
 import * as types from '../utils/types/chatTypes';
-import { dispatch } from 'rxjs/internal/observable/pairs';
 
-const CHAT_MESSAGES = firebaseService.database().ref('Messages');
-const CHAT_MESSAGES_LIMIT = 20;
+// const CHAT_MESSAGES = firebaseService.database().ref('Messages');
+// const CHAT_MESSAGES_LIMIT = 20;
 
 const chatMessageLoading = createAction(types.CHAT_MESSAGE_LOADING);
 const chatMessageSuccess = createAction(types.CHAT_MESSAGE_SUCCESS);
 const chatMessageError = createAction(types.CHAT_MESSAGE_ERROR, 'error');
-const chatUpdateMessage = createAction(types.CHAT_MESSAGE_UPDATE, 'text');
 const loadMessagesSuccess = createAction(types.CHAT_LOAD_MESSAGES_SUCCESS, 'messages');
 const loadMessagesError = createAction(types.CHAT_LOAD_MESSAGES_ERROR, 'error');
 
-const loadMessages = createActionThunk((_, dispatch) => {
-  CHAT_MESSAGES.limitToLast(CHAT_MESSAGES_LIMIT).on('value',
-    messages => {
-      dispatch(loadMessagesSuccess(messages.val()));
-    }, err => {
-      dispatch(loadMessagesError(err.message));
-    });
-});
+// const loadMessages = createActionThunk((_, dispatch) => {
+//   CHAT_MESSAGES.limitToLast(CHAT_MESSAGES_LIMIT).on('value',
+//     messages => {
+//       dispatch(loadMessagesSuccess(messages.val()));
+//     }, err => {
+//       dispatch(loadMessagesError(err.message));
+//     });
+// });
 
-const sendMessage = createActionThunk((message, dispatch) => {
+const sendMessage = createActionThunk((props, dispatch) => {
   dispatch(chatMessageLoading());
 
   const currentUser = firebaseService.auth().currentUser;
   const createAt = new Date().getTime();
   const chatMessage = {
-    text: message,
+    text: props.message,
     createAt,
     user: {
-      id: currentUser.id,
+      id: currentUser.uid,
       email: currentUser.email
     }
   }
 
-  CHAT_MESSAGES.push().set(chatMessage, err => {
-    if (err) {
-      dispatch(chatMessageError(err.message));
-    } else {
-      dispatch(chatMessageSuccess());
-    }
-  })
+  firebaseService
+    .database()
+    .ref('Messages')
+    .push().set(chatMessage, err => {
+      if (err) {
+        dispatch(chatMessageError(err.message));
+      } else {
+        dispatch(chatMessageSuccess());
+      }
+      props.callback();
+    })
 
 })
 
+const loadMessages = createActionThunk((_, dispatch) => { });
 
-export { loadMessages, sendMessage}
+
+export { loadMessages, sendMessage }
--- src/views/screens/chat/chatInput.js
+++ src/views/screens/chat/chatInput.js
@@ -1,9 +1,11 @@
 import React, { PureComponent } from 'react'
-import { View } from 'react-native';
-import { Item as FormItem, Input, Icon, Button } from 'native-base';
+import { View } from 'react-native'
+import { Item as FormItem, Input, Icon, Button } from 'native-base'
 import PropTypes from 'prop-types'
-import translations from '../../../i18n';
-import * as styles from '../../styles/style';
+import translations from '../../../i18n'
+import * as styles from '../../styles/style'
+import { connect } from 'react-redux'
+import { sendMessage } from '../../../store/actions/chatActions'
 
 class ChatInput extends PureComponent {
 
@@ -16,17 +18,24 @@ class ChatInput extends PureComponent {
   }
 
   onButtomPress = () => {
+    props = {
+      message: this.state.message,
+      callback: () => {
+        this.setState({message: ''});
+      }
+    }
 
+    this.props.sendMessage(props);
   }
 
   render() {
     const disable = this.props.sending || this.state.message.trim().length == 0;
-    const disableStyle = disable?{backgroundColor: '#f0ece2'}:{};
-    const disbleBottom = disable?{color: 'red'}:{};
+    const disableStyle = disable ? { backgroundColor: '#f0ece2' } : {};
+    const disbleBottom = disable ? { color: 'red' } : {};
 
     return (
       <View style={styles.chat.chatInputContainer}>
-        <View style={{width: '80%'}} >
+        <View style={{ width: '80%' }} >
           <FormItem rounded>
             <Input
               style={styles.chat.chatInputMessage}
@@ -35,25 +44,23 @@ class ChatInput extends PureComponent {
               onChangeText={this.onMessageChange}
               underlineColorAndroid={'transparent'}
               editable={true}
-              returnKeyType='send' 
-              />
+              returnKeyType='send'
+            />
           </FormItem>
         </View>
         <View style={[styles.chat.chatInputBottomBackground, disableStyle]}>
           <Button transparent disabled={disable} onPress={this.onButtomPress} >
-            <Icon name='send' style={[{color: 'green'}, disbleBottom]} />
+            <Icon name='send' style={[{ color: 'green' }, disbleBottom]} />
           </Button>
-          
+
         </View>
       </View>
     )
   }
 }
 
+const map = ({ chat }) => ({
+  sending: chat.sending,
+});
 
-// ChatInput.prototype = {
-//   sendMessage: PropTypes.func.isRequired,
-//   sending: PropTypes.bool.isRequired,
-// }
-
-export default ChatInput;
+export default connect(map, { sendMessage })(ChatInput);
